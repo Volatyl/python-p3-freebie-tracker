ec270c535534b77159e96b8bc3d04a70b02db1d3
Kennedy Muinde <kennedymuinde@gmail.com>
2023-05-27 21:34:45 +0300

    Completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table, create_engine
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,6 +8,17 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+company_dev = Table(
+    'company_dev', Base.metadata,
+    Column('company_id', ForeignKey('companies.id')),
+    Column('dev_id', ForeignKey('devs.id'))
+)
+
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,14 +26,82 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    devs = relationship('Dev', secondary='company_dev',
+                        back_populates='companies')
+    freebies = relationship('Freebie', back_populates='company')
+
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return self.name
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(item_name=item_name, value=value)
+
+        self.freebies.append(freebie)
+        dev.freebies.append(freebie)
+
+        session.add(freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(cls):
+        return session.query(Company).order_by(Company.founding_year).first()
+
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+
+    companies = relationship(
+        'Company', secondary='company_dev', back_populates='devs')
+    freebies = relationship('Freebie', back_populates='dev')
 
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f'<Dev: {self.name}>'
+
+    def received_one(self, item_name):
+        for freebie in self.freebies:
+            if item_name == freebie.item_name:
+                return True
+        return False
+
+    def give_away(self, dev, freebie):
+        if self.name == freebie.dev.name:
+            freebie.dev = dev
+            session.commit()
+            return 'Done'
+        return False
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    dev = relationship('Dev', back_populates='freebies')
+    company = relationship('Company', back_populates='freebies')
+
+    def __repr__(self):
+        return self.item_name
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
+
+
+Sam = session.query(Dev).filter_by(name='Sam').first()
+Ken = session.query(Dev).filter_by(name='Ken').first()
+folio = session.query(Freebie).filter_by(item_name='folio').first()
+Tv = session.query(Freebie).filter_by(item_name='Tv').first()
+
+print(Ken.give_away(Sam, folio))
+
+
+# Tesla = session.query(Company).filter_by(name='Tesla').first()
+
+# truck = Tesla.give_freebie(Sam, 'Truck', 6345)
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,48 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Dev, Company, Freebie, company_dev
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///freebies.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # session.query(Dev).delete()
+    # session.query(Company).delete()
+    # session.query(Freebie).delete()
+
+    # HP = Company(name='HP', founding_year=1996)
+    # Samsung = Company(name='Samsung', founding_year=1998)
+    # Tesla = Company(name='Tesla', founding_year=2001)
+
+    # Ken = Dev(name='Ken')
+    # Sam = Dev(name='Sam')
+
+    # session.add_all([HP, Samsung, Tesla, Ken, Sam])
+    # session.commit()
+
+    # mouse = Freebie(item_name="mouse", value=40, dev=Ken, company=HP)
+    # car = Freebie(item_name="car", value=23000, dev=Ken, company=Tesla)
+    # galaxy10 = Freebie(item_name="galaxy10", value=40,
+    #                    dev=Sam, company=Samsung)
+    # folio = Freebie(item_name="folio", value=340, dev=Sam, company=HP)
+
+    # session.add_all([mouse, car, galaxy10, folio])
+    # session.commit()
+
+    # HP.devs.append(Ken)
+    # HP.devs.append(Sam)
+    # Samsung.devs.append(Sam)
+    # Tesla.devs.append(Ken)
+
+    # session.commit()
+
+    # x = session.query(Freebie).filter_by(item_name='mouse').first()
+
+    # print(x.print_details())
+
+    
